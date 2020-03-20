6e973ff1b33b76426dfaf5c6146cb65f41703379
Srilal Sachintha <37685396+SrilalS@users.noreply.github.com>
2020-03-20 22:47:28 +0530

    Update Engine.js

--- Engine.js
+++ Engine.js
@@ -10,7 +10,13 @@ var nc;
   $.ajax(settings).done(function (response) {
     console.log(response);
     td = response['data']['local_deaths'];
-    tc = response['data']['local_deaths'];
+    tc = response['data']['local_total_cases'];
     nc = response['data']['local_new_cases'];
+    console.log(nc);
     console.log(tc);
+    console.log(td);
+
+    document.getElementById('totalcases').innerHTML = tc;
+    document.getElementById('newcases').innerHTML = nc;
+    document.getElementById('deaths').innerHTML = td;
   });
