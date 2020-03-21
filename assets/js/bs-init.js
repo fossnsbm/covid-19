9602df763da28161db1f026205718a8f6e768135
Harindu973 <harindulakshankob@gmail.com>
2020-03-21 16:21:11 +0530

    Master Branch Updated

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-	AOS.init({ disable: 'mobile' });
 	$('[data-bs-hover-animate]')
 		.mouseenter( function(){ var elem = $(this); elem.addClass('animated ' + elem.attr('data-bs-hover-animate')) })
 		.mouseleave( function(){ var elem = $(this); elem.removeClass('animated ' + elem.attr('data-bs-hover-animate')) });
